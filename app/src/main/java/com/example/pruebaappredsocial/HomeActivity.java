a4b548686ccc25abb973233a193d5c8a26fddd0d
AGEJH <ajimenez8@ucol.mx>
2024-08-09 16:09:20 -0600

    Se añadio el xml del menú de navegación y se adjuntó en los xml de las diferentes vistas dentro de los diferentes apartados de la red social, Faltando solo crear la vista de videos y adjuntandole por ultimo el menú de navegación. (Ojo: no se le ha aplicado la logica de navegación al menú)

--- app/src/main/java/com/example/pruebaappredsocial/HomeActivity.java
+++ app/src/main/java/com/example/pruebaappredsocial/HomeActivity.java
@@ -15,10 +15,8 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
